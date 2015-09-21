a77b4e43cea4d8c241a82b01e49231bdf789cc3e
Gjohnson42 <gjohnson2718@gmail.com>
2015-09-20 20:48:18 -0400

    Revert "This will add an extra sentence."
    This reverts commit 3a552bf3b2b796880594bf40584f87f5be92ef53.
    
    Conflicts:
    	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hiya there.");
+<<<<<<< HEAD
         System.out.println("This is Rob here.");
+=======
+>>>>>>> parent of 3a552bf... This will add an extra sentence.
     }
 }
